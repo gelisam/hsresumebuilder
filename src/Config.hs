712a837bc60de21bbf7fe638ec8addac4da94f5d
Josep Bigorra <josep.bigorra@ingka.ikea.com>
2022-04-12 23:24:53 +0200

    Initial implementation of resume builder

--- src/Config.hs
+++ src/Config.hs
@@ -4,21 +4,45 @@
 module Config where
 
 import qualified Data.ByteString
+import Data.Map (Map)
 import qualified Data.Text
 import Data.Text.Encoding (encodeUtf8)
 import Data.Yaml (FromJSON, ToJSON, decodeFileThrow)
 import GHC.Generics (Generic)
 
-data HSResumeBuilderPreferences = HSResumeBuilderPreferences
-  { personalInformation :: HSResumeBuilderPersonalInfo
+data Preferences = Preferences
+  { personalInformation :: PersonalInfo,
+    appearancePreferences :: AppearancePreferences
   }
   deriving (Generic, Show, ToJSON, FromJSON)
 
-data HSResumeBuilderPersonalInfo = HSResumeBuilderPersonalInfo
-  { contactLines :: [String]
+data PersonalInfo = PersonalInfo
+  { displayName :: String,
+    jobTitle :: String,
+    addressLines :: [String],
+    contactInformation :: PersonalInfoContactInfo
   }
   deriving (Generic, Show, ToJSON, FromJSON)
 
-getConfig :: IO (Maybe HSResumeBuilderPreferences)
+data PersonalInfoContactInfo = PersonalInfoContactInfo
+  { phoneNumbers :: [String],
+    emails :: [String],
+    websites :: [String]
+  }
+  deriving (Generic, Show, ToJSON, FromJSON)
+
+data AppearancePreferences = AppearancePreferences
+  { theme :: String,
+    themeSections :: AnachronicThemeSections
+  }
+  deriving (Generic, Show, ToJSON, FromJSON)
+
+data AnachronicThemeSections = AnachronicThemeSections
+  { shortIntroSectionBody :: String,
+    shortIntroSectionTitle :: String
+  }
+  deriving (Generic, Show, ToJSON, FromJSON)
+
+getConfig :: IO (Maybe Preferences)
 getConfig = do
-  Data.Yaml.decodeFileThrow ".hsresumebuilder.yaml" :: IO (Maybe HSResumeBuilderPreferences)
+  Data.Yaml.decodeFileThrow ".hsresumebuilder.yaml" :: IO (Maybe Preferences)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc,
+  )
+where
+
+import Config (PersonalInfo (addressLines, contactInformation, jobTitle), PersonalInfoContactInfo (emails, phoneNumbers, websites), Preferences (Preferences, personalInformation), displayName, getConfig)
+import Control.Monad (forM_)
+import System.Exit (exitFailure)
+import System.IO
+  ( IOMode (ReadWriteMode),
+    hClose,
+    hGetContents,
+    openFile,
+  )
+import Text.Blaze.Html.Renderer.Pretty (renderHtml)
+import Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes as A
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  maybeParsedConfig <- getConfig
+  maybe exitFailure beginRendering maybeParsedConfig
+
+beginRendering :: Preferences -> IO ()
+beginRendering config = do
+  let generatedContent = renderHtml . anachronicTheme $ config
+  saveContentToFile generatedContent
+  putStrLn generatedContent
+
+saveContentToFile :: String -> IO ()
+saveContentToFile content = do
+  handle <- openFile "output.html" ReadWriteMode
+  contents <- hGetContents handle
+  hClose handle
+  writeFile "output.html" content
+
+anachronicTheme :: Preferences -> Html
+anachronicTheme config = docTypeHtml $ do
+  H.head $ do
+    H.title . toHtml $ ("Resume of " ++ (displayName . personalInformation $ config))
+    link ! rel "stylesheet" ! href "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"
+  body $ do
+    h1 . toHtml . displayName . personalInformation $ config
+    (h2 ! A.style "color: darkgreen;") . toHtml . jobTitle . personalInformation $ config
+    H.div $ do
+      ul $ forM_ (phoneNumbers . contactInformation . personalInformation $ config) (p . toHtml)
+      ul $ forM_ (emails . contactInformation . personalInformation $ config) (p . toHtml)
+      ul $ forM_ (websites . contactInformation . personalInformation $ config) (p . toHtml)
+    H.div $ do
+      ul
