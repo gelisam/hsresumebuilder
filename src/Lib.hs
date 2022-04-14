b6914a2a07ec26489e2886f2584109886490bbb5
averageflow <jjbigorra@gmail.com>
2022-04-14 21:54:43 +0200

    nice

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,7 +5,8 @@ module Lib
   )
 where
 
-import Config (PersonalInfo (addressLines, contactInformation, jobTitle), PersonalInfoContactInfo (emails, phoneNumbers, websites), Preferences (Preferences, personalInformation), displayName, getConfig)
+import Config
+import Config (AnachronicThemeSections (shortIntroSectionBody))
 import Control.Monad (forM_)
 import System.Exit (exitFailure)
 import System.IO
@@ -40,11 +41,19 @@ anachronicTheme :: Preferences -> Html
 anachronicTheme config = docTypeHtml $ do
   H.head $ do
     H.title . toHtml $ ("Resume of " ++ (displayName . personalInformation $ config))
-    link ! rel "stylesheet" ! href "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"
+    link ! rel "stylesheet" ! href "https://cdn.jsdelivr.net/npm/water.css@2/out/light.css"
   body $ do
-    h1 . toHtml . displayName . personalInformation $ config
-    (h2 ! A.style "color: darkgreen;") . toHtml . jobTitle . personalInformation $ config
-    H.div $ do
-      ul $ forM_ (phoneNumbers . contactInformation . personalInformation $ config) (p . toHtml)
-      ul $ forM_ (emails . contactInformation . personalInformation $ config) (p . toHtml)
-      ul $ forM_ (websites . contactInformation . personalInformation $ config) (p . toHtml)
+    main $ do
+      h1 . toHtml . displayName . personalInformation $ config
+      (h2 ! A.style "color: darkgreen;") . toHtml . jobTitle . personalInformation $ config
+      H.div ! A.style "display: flex" $ do
+        H.div ! A.style "padding: 1em;" $ do
+          forM_ (phoneNumbers . contactInformation . personalInformation $ config) (p . toHtml)
+          forM_ (emails . contactInformation . personalInformation $ config) (p . toHtml)
+          forM_ (websites . contactInformation . personalInformation $ config) (p . toHtml)
+        H.div ! A.style "padding: 1em;" $ do
+          forM_ (addressLines . personalInformation $ config) (p . toHtml)
+
+      H.div $ do
+        h3 . toHtml $ (shortIntroSectionTitle . themeSections . appearancePreferences $ config)
+        (p ! A.style "text-align: justify") . toHtml $ (shortIntroSectionBody . themeSections . appearancePreferences $ config)
